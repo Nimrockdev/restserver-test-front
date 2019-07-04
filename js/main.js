96e0651cd1a8ca0238b1f1fdb8fe0f691bcbaa41
Javier <jsg8405@gmail.com>
2019-07-04 15:31:50 +0200

    update code test

--- js/main.js
+++ js/main.js
@@ -49,10 +49,67 @@ boton.addEventListener('click', function() {
         })
         .then(function(res) {
             if (res.status == 200) {
-                console.log(res.data);
-                mensaje.innerHTML = JSON.stringify(res.data, undefined, 2);
+                // mensaje.innerHTML = JSON.stringify(res.data, undefined, 2);
+
+                // function genera_tabla() {
+                // Obtener la referencia del elemento body
+
+                // let salario = JSON.stringify(res.data);
+                let salario = (res.data);
+                console.log(salario);
+
+                var body = document.getElementsByTagName("body")[0];
+
+                // Crea un elemento <table> y un elemento <tbody>
+                var tabla = document.createElement("table");
+                var tblBody = document.createElement("tbody");
+
+
+                for (i in salario) {
+                    x = salario[i].id + ' ' + salario[i].salario;
+                    console.log(x);
+                }
+                var textoCelda = '';
+                // Crea las celdas
+                for (var i = 0; i < 10; i++) {
+                    // Crea las hileras de la tabla
+                    var hilera = document.createElement("tr");
+
+                    for (var j = 0; j < 2; j++) {
+                        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+                        // texto sea el contenido de <td>, ubica el elemento <td> al final
+                        // de la hilera de la tabla
+                        var celda = document.createElement("td");
+                        if (j = 1) {
+                            textoCelda = document.createTextNode(salario[i].id);
+                            console.log(salario[i].id);
+                        }
+                        if (j = 2) {
+                            textoCelda = document.createTextNode(salario[i].salario);
+                            console.log(salario[i].salario);
+                        }
+
+                        // var textoCelda = document.createTextNode(salario[i].salario);
+
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                    }
+
+                    // agrega la hilera al final de la tabla (al final del elemento tblbody)
+                    tblBody.appendChild(hilera);
+                }
+
+                // posiciona el <tbody> debajo del elemento <table>
+                tabla.appendChild(tblBody);
+                // appends <table> into <body>
+                body.appendChild(tabla);
+                // modifica el atributo "border" de la tabla y lo fija a "2";
+                tabla.setAttribute("border", "2");
             }
-            console.log(res);
+
+            // }
+            // console.log(res);
+            // console.log(stock);
         })
         .catch(function(err) {
             console.log(err);
@@ -70,10 +127,11 @@ boton.addEventListener('click', function() {
         })
         .then(function(res) {
             if (res.status == 200) {
-                console.log(res);
-                mensaje.innerHTML = res.data;
+                // console.log(res);
+                mensaje.innerHTML = JSON.stringify(res.data, undefined, 2);
+                //mensaje.innerHTML = res.data.JSON;
             }
-            console.log(res);
+            console.log(res.data);
         })
         .catch(function(err) {
             console.log(err);
