0b76c7f37868bd4e3b8a00612a8911a69d82c6c7
Javier <jsg8405@gmail.com>
2019-07-10 16:24:19 +0200

    add button Mongo products

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 var loading = document.getElementById('loading');
 var mensaje = document.getElementById('mensaje');
 
-let salarios = [
-    { id: 1, salario: 1000 },
-    { id: 2, salario: 2000 },
-    { id: 3, salario: 3000 }
-];
 
+// let salarios = [
+//     { id: 1, salario: 1000 },
+//     { id: 2, salario: 2000 },
+//     { id: 3, salario: 3000 }
+// ];
 
-
-// console.log(salarios);
-// for (contador = 0; contador < 7; contador++) {
-//     document.write("El valor de la posición [" + contador + "] es [" + aDias[contador] + "]<br/>");
-// }
-
-// for (contador = 0; salarios.length < 7; contador++) {
-//     document.write("El valor de la posición [" + contador + "] es [" + salarios[contador] + "]<br/>");
-//     document.write(salarios[contador]);
-// }
 
 var boton = document.getElementById('helloWorld');
 boton.addEventListener('click', function() {
@@ -40,6 +30,7 @@ boton.addEventListener('click', function() {
             loading.style.display = 'none';
         });
 });
+
 
 var boton = document.getElementById('salarios');
 boton.addEventListener('click', function() {
@@ -119,6 +110,7 @@ boton.addEventListener('click', function() {
         });
 });
 
+
 var boton = document.getElementById('getTimeServiceData');
 boton.addEventListener('click', function() {
     loading.style.display = 'block';
@@ -141,12 +133,14 @@ boton.addEventListener('click', function() {
         });
 });
 
+
 var boton = document.getElementById('location');
 let config = {
     headers: {
         location: "Castelldefels",
     }
 }
+
 
 boton.addEventListener('click', function() {
     loading.style.display = 'block';
@@ -170,6 +164,7 @@ boton.addEventListener('click', function() {
         });
 });
 
+
 var boton = document.getElementById('getTime');
 boton.addEventListener('click', function() {
     loading.style.display = 'block';
@@ -183,6 +178,29 @@ boton.addEventListener('click', function() {
                 mensaje.innerHTML = JSON.stringify(res.data, undefined, 2);
             }
             console.log(res);
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+        .then(function() {
+            loading.style.display = 'none';
+        });
+});
+
+var boton = document.getElementById('product');
+boton.addEventListener('click', function() {
+    loading.style.display = 'block';
+    axios.get('https://restserver-test-jsg.herokuapp.com/products/', {
+            responseType: 'json',
+
+        })
+        .then(function(res) {
+            if (res.status == 200) {
+                console.log(res.data);
+                let productos = res.data;
+                mensaje.innerHTML = JSON.stringify(productos, undefined, 2);
+            }
+            // console.log(res);
         })
         .catch(function(err) {
             console.log(err);
